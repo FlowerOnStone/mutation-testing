5af9c4b52d60aed71b0f71a91607b64763a16b48
FlowerOnStone <thuanbn03@gmail.com>
2022-11-07 10:26:14 +0700

    add mutpy result

--- mutpy testing/getUnittestTestcase.py
+++ mutpy testing/getUnittestTestcase.py
@@ -16,44 +16,86 @@ def used_unittest(filename):
     return False
 
 
-filename_tests = os.listdir(TEST_ROOT)
-filename_tests = [filename for filename in filename_tests if filename.startswith("test_") and filename.endswith(".py")]
-print(len(filename_tests))
+def get_correct_testcase():
+    correct_testcase_file = open(os.path.join(TEST_ROOT, "correct_testcase.txt"))
+    lines = correct_testcase_file.readlines()
+    correct_testcase_file.close()
+    correct_testcase = {line.split()[0]: float(line.split()[1]) for line in lines if float(line.split()[1]) <= 1}
+    return correct_testcase
 
-filename_codes = os.listdir(CODE_ROOT)
-filename_codes = [filename for filename in filename_codes if ".py" in filename]
-print(len(filename_codes))
 
-tmp = []
-for filename_test in filename_tests:
-    flag = False
-    for filename_code in filename_codes:
-        if filename_code[0:-3] in filename_test:
-            flag = True
-            break
-    if flag:
-        tmp.append(filename_test)
+def get_test_file():
+    correct_testcase = get_correct_testcase()
+    test_files = os.listdir(TEST_ROOT)
+    test_files = [file_name for file_name in test_files
+                  if file_name.startswith("test_") and file_name.endswith(".py") and file_name in correct_testcase]
+    code_files = os.listdir(CODE_ROOT)
+    code_files = [file_name for file_name in code_files if ".py" in file_name]
 
-filename_tests = [filename for filename in tmp]
-tmp = []
+    tmp = []
+    for test_file in test_files:
+        flag = False
+        for code_file in code_files:
+            if code_file[0:-3] in test_file and used_unittest(test_file):
+                flag = True
+                break
+        if flag:
+            tmp.append(test_file)
 
-for i in range(len(filename_tests)):
-    if used_unittest(filename_tests[i]):
-        tmp.append(filename_tests[i])
+    test_files = [file_name for file_name in tmp]
+    result = {}
+    for test_file in test_files:
+        for code_file in code_files:
+            if "test_" + code_file in test_file:
+                if not (code_file in result):
+                    result[code_file] = {test_file: correct_testcase[test_file]}
+                else:
+                    result[code_file][test_file] = correct_testcase[test_file]
+    return result
 
-filename_tests = tmp
 
-code_test = {}
+def copy_file(original_path, target_path, filename):
+    original_path = os.path.join(original_path, filename)
+    target_path = os.path.join(target_path, filename)
+    shutil.copy(original_path, target_path)
 
-for filename_test in filename_tests:
-    flag = False
-    for filename_code in filename_codes:
-        if "test_" + filename_code in filename_test:
-            if not (filename_code in code_test):
-                code_test[filename_code] = [filename_test]
-            else:
-                code_test[filename_code].append(filename_test)
 
-for code in code_test:
-    for test in code_test[code]:
-        original = os.path.join()
+def get_test_report():
+    log_file = open("log.txt", "r")
+    test_report = log_file.readline().split()
+    log_file.close()
+    return test_report
+
+
+def run_mutpy():
+    report = open("report.csv", "w")
+    print("filename,all,killed,survived,incompetent,timeout", file=report)
+    test_files = get_test_file()
+    for code_file in test_files:
+        for test_file in test_files[code_file]:
+            copy_file(TEST_ROOT, r".", test_file)
+            copy_file(CODE_ROOT, r".", code_file)
+            print(code_file)
+            script = "mut.py --target " + code_file + " --unit-test " + test_file \
+                     + " --timeout-factor " + str(test_files[code_file][test_file] + 0.2)
+            os.system(script)
+            os.system("mv mutpy_running.txt mutpy\\ result/" + test_file[0:-3] + ".txt")
+            os.remove(code_file)
+            os.remove(test_file)
+    report.close()
+
+
+def run_unittest():
+    test_files = get_test_file()
+    for code_file in test_files:
+        for test_file in test_files[code_file]:
+            copy_file(TEST_ROOT, r".", test_file)
+            copy_file(CODE_ROOT, r".", code_file)
+            print(test_file)
+            os.system("python3 -m unittest " + test_file)
+            print()
+            os.remove(code_file)
+            os.remove(test_file)
+
+
+run_mutpy()
